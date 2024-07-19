0691679f7087a1e8ef281d742634bec8914dc48e
Alejandro Castellon <alejandrocastellonfer@gmail.com>
2024-07-19 01:21:50 -0400

    implement functionallity control automatico

--- src/app/dashboard/autoclave/automatico/page.tsx
+++ src/app/dashboard/autoclave/automatico/page.tsx
@@ -1,3 +1,6 @@
+"use client";
+
+import { useState, useEffect } from "react";
 import { Button } from "@/components/ui/button";
 import {
   Card,
@@ -6,15 +9,55 @@ import {
   CardTitle,
   CardFooter,
 } from "@/components/ui/card";
+import {
+  Dialog,
+  DialogContent,
+  DialogDescription,
+  DialogFooter,
+  DialogHeader,
+  DialogTitle,
+  DialogTrigger,
+  DialogClose,
+} from "@/components/ui/dialog";
 import { Gauge, Thermometer, Timer } from "lucide-react";
 
 export default function Page() {
+  const [totalTime, setTotalTime] = useState(0);
+  const [isRunning, setIsRunning] = useState(false);
+
+  useEffect(() => {
+    let timer: NodeJS.Timeout;
+    if (isRunning) {
+      timer = setInterval(() => {
+        setTotalTime((prevTime) => prevTime + 1);
+      }, 1000);
+    }
+    return () => clearInterval(timer);
+  }, [isRunning]);
+
+  const handleStart = () => {
+    setIsRunning(true);
+  };
+
+  const handleCancel = () => {
+    setIsRunning(false);
+    setTotalTime(0);
+  };
+
+  const formatTime = (seconds: number): string => {
+    const minutes = Math.floor(seconds / 60);
+    const secs = seconds % 60;
+    return `${minutes.toString().padStart(2, "0")}:${secs
+      .toString()
+      .padStart(2, "0")}`;
+  };
+
   return (
     <main>
       <div className="text-3xl font-bold mb-4">Control Automático</div>
       <Card>
         <CardContent className="flex items-center justify-center bg-sky-100 m-4">
-          <div className="text-5xl font-bold mt-3">00.00</div>
+          <div className="text-5xl font-bold mt-3">{formatTime(totalTime)}</div>
         </CardContent>
         <CardFooter className="flex justify-center">
           <div className="flex text-2xl font-bold items-center">
@@ -52,8 +95,33 @@ export default function Page() {
           <div className="text-5xl font-bold mt-2">35.00 </div>
         </CardContent>
       </Card>
-      <Button className="m-4 bg-sky-500">Comenzar</Button>
-      <Button className="m-4 bg-sky-500">Cancelar</Button>
+      <Button className="m-4 bg-sky-500" onClick={handleStart}>
+        Comenzar
+      </Button>
+      <Dialog>
+        <DialogTrigger asChild>
+          <Button className="m-4 bg-sky-500">Cancelar</Button>
+        </DialogTrigger>
+        <DialogContent className="sm:max-w-[425px]">
+          <DialogHeader>
+            <DialogTitle>Cancelar</DialogTitle>
+            <DialogDescription>
+              Esta seguro que quiere parar el ciclo.
+            </DialogDescription>
+          </DialogHeader>
+          <DialogFooter className="sm:justify-start">
+            <DialogClose asChild>
+              <Button
+                type="button"
+                variant="destructive"
+                onClick={handleCancel}
+              >
+                Parar el ciclo
+              </Button>
+            </DialogClose>
+          </DialogFooter>
+        </DialogContent>
+      </Dialog>
       <img src="/charts.svg" width={300} height={500} />
     </main>
   );
