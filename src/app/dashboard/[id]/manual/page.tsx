28fd39ae0aefacbb379f3f405ceafdd102098468
Alejandro Castellon <alejandrocastellonfer@gmail.com>
2024-10-21 10:14:12 -0400

    implement control manual mqtt

--- src/app/dashboard/[id]/manual/page.tsx
+++ src/app/dashboard/[id]/manual/page.tsx
@@ -5,17 +5,64 @@ import { Button } from "@/components/ui/button";
 import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card";
 import { Gauge, Thermometer, Circle, CircleCheckBig } from "lucide-react";
 import { supabase } from "@/utils/supabaseClient";
+import { connectMqttClient } from "@/app/api/mqtt";
 
 export default function Page({ params }: { params: { id: string } }) {
   const [isDoorOpen, setIsDoorOpen] = useState(false);
   const [agua, setAgua] = useState(false);
   const [type, setType] = useState<number>();
+  const [client, setClient] = useState<any>(null);
+  const [chartPress, setChartPress] = useState<{ time: any; value: number }[]>(
+    []
+  );
+  const [chartTemp, setChartTemp] = useState<{ time: any; value: number }[]>(
+    []
+  );
+
+  useEffect(() => {
+    const mqttClient = connectMqttClient(); // Conectar cliente MQTT
+    setClient(mqttClient);
+
+    return () => {
+      if (mqttClient) {
+        mqttClient.end(); // Cerrar conexión al desmontar el componente
+      }
+    };
+  }, []);
+
+  useEffect(() => {
+    if (client) {
+      client.on("message", (topic: string, message: Buffer) => {
+        const payload = JSON.parse(message.toString());
+        console.log("Mensaje recibido:", payload);
+
+        const currentTime = new Date().getTime();
+
+        setChartPress((prevData) => [
+          ...prevData,
+          { time: currentTime, value: payload.presion },
+        ]);
+        setChartTemp((prevData) => [
+          ...prevData,
+          { time: currentTime, value: payload.temperatura },
+        ]);
+      });
+    }
+  }, [client]);
 
   const handleDoorOpenClick = () => {
-    setIsDoorOpen(true);
+    if (client) {
+      // Publicar un mensaje al ESP para que comience a enviar datos
+      client.publish("autoclaves/control", "start");
+      setIsDoorOpen(true);
+    }
   };
   const handleDoorCloseClick = () => {
-    setIsDoorOpen(false);
+    if (client) {
+      // Publicar un mensaje al ESP para que comience a enviar datos
+      client.publish("autoclaves/control", "stop");
+      setIsDoorOpen(false);
+    }
   };
   const handleAguaClick = () => {
     setAgua(!agua);
@@ -82,10 +129,7 @@ export default function Page({ params }: { params: { id: string } }) {
           {type === 4 && (
             <div className="grid grid-cols-2 gap-4">
               <Button className="w-full text-lg py-3 bg-sky-500">
-                EV Filtro Aire
-              </Button>
-              <Button className="w-full text-lg py-3 bg-sky-500">
-                Bomba vacio
+                EV Despresurizacion
               </Button>
             </div>
           )}
@@ -132,7 +176,7 @@ export default function Page({ params }: { params: { id: string } }) {
 
         {/* Sección de tarjetas */}
         <div className="flex flex-col space-y-4 lg:space-y-4 lg:w-3/4 lg:ml-auto mt-10">
-          <Card>
+          <Card className="flex-1">
             <CardHeader className="pb-0">
               <CardTitle className="flex items-center justify-between">
                 <div className="flex">
@@ -143,11 +187,14 @@ export default function Page({ params }: { params: { id: string } }) {
               </CardTitle>
             </CardHeader>
             <CardContent className="flex items-center justify-center bg-sky-100 m-4">
-              <div className="text-5xl font-bold mt-2">5.00</div>
+              <div className="text-5xl font-bold mt-2">
+                {chartPress.length > 0
+                  ? chartPress[chartPress.length - 1].value.toFixed(2)
+                  : "0.00"}
+              </div>
             </CardContent>
           </Card>
-
-          <Card>
+          <Card className="flex-1">
             <CardHeader className="pb-0">
               <CardTitle className="flex items-center justify-between">
                 <div className="flex">
@@ -158,7 +205,11 @@ export default function Page({ params }: { params: { id: string } }) {
               </CardTitle>
             </CardHeader>
             <CardContent className="flex items-center justify-center bg-sky-100 m-4">
-              <div className="text-5xl font-bold mt-2">35.00</div>
+              <div className="text-5xl font-bold mt-2">
+                {chartTemp.length > 0
+                  ? chartTemp[chartTemp.length - 1].value.toFixed(2)
+                  : "0.00"}
+              </div>
             </CardContent>
           </Card>
           {(type === 2 || type === 4) && (
